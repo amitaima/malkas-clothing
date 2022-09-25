01d07af54c8a8df2d16388f4b8601ac7c0aaeaaf
Amitai Malka <amitai.malka@gmail.com>
2022-09-25 14:41:58 +0300

    SignUp

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,26 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = createUserDocFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with google</button>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUpForm></SignUpForm>
     </div>
   );
 };
