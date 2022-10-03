f383782cfc9f5692252a66d0abaa7102e2f4395c
Amitai Malka <amitai.malka@gmail.com>
2022-10-03 16:58:47 +0300

    Improvment of UI UX

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -1,17 +1,23 @@
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
   const { setIsCartOpen, cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
   const close = () => {
     setIsCartOpen(false);
   };
   const open = () => {
     setIsCartOpen(true);
+  };
+
+  const goToCheckout = () => {
+    navigate("/checkout");
+    close();
   };
 
   return (
@@ -25,9 +31,9 @@ const CartDropdown = () => {
           return <CartItem key={item.id} cartItem={item} />;
         })}
       </div>
-      <Link className="link-to-checkout" to="/checkout">
-        <Button>Go to checkout</Button>
-      </Link>
+      {/* <Link className="link-to-checkout" to="/checkout"> */}
+      <Button onClick={goToCheckout}>Go to checkout</Button>
+      {/* </Link> */}
     </div>
   );
 };
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,35 @@
 import "./cart-item.styles.scss";
+import { GrTrash } from "react-icons/gr";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartItem = ({ cartItem }) => {
   const { name, price, imageUrl, quantity } = cartItem;
+  const { removeItemFromCart } = useContext(CartContext);
 
   return (
     <div className="cart-item-container">
-      <img src={imageUrl} alt={`Product photo of a ${name}`} />
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <div className="price-and-quantity">
-          <span className="price">${price}</span>
-          <span className="quantity">QTY: {quantity}</span>
+      <div className="cart-item-info-container">
+        <div className="cart-image-container">
+          <img src={imageUrl} alt={`Product photo of a ${name}`} />
         </div>
-        <span className="quantity">Subtotal: ${quantity * price}</span>
+        <div className="item-details">
+          <span className="name">{name}</span>
+          <div className="price-and-quantity">
+            <span className="price">${price}</span>
+            <span className="quantity">QTY: {quantity}</span>
+          </div>
+          <span className="quantity">Subtotal: ${quantity * price}</span>
+        </div>
+      </div>
+      <div className="div-trash-icon">
+        <FaRegTrashAlt
+          onClick={() => {
+            removeItemFromCart(cartItem);
+          }}
+          className="trash-icon"
+        />
       </div>
     </div>
   );
