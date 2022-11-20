256820000bf48afeeb72e529805bb5d005f6e39e
Amitai Malka <amitai.malka@gmail.com>
2022-11-20 18:16:04 +0200

    added PWS support

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import "./general.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -27,6 +28,7 @@ root.render(
   // </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
